076dfc4a83c6ab30c74065501cc559e25d8a9b6b
dpwls0329 <dpwls0329@kakao.com>
2022-06-20 17:09:50 +0900

    up

---
+++ package2/OS.java
@@ -0,0 +1,7 @@
+package study.pkg002;
+
+public class OS {
+    enum os {
+        MAC, WINDOWS, DEVIAN, UNIX, UBUNTU, CENT
+    }
+}
---
+++ package2/Ram.java
@@ -0,0 +1,15 @@
+package study.pkg002;
+
+public class Ram {
+    
+    public int memory;
+
+    public Ram() {
+        this.memory = 4;
+    }
+
+    public Ram(int ram){
+        this.memory = ram;
+    }
+
+}
